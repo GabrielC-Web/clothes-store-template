762a0f37c4aba1b2eb6d3832ce06a18b8ae1ea54
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-19 12:59:45 -0400

    Creados módulos principales

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainLayoutComponent,
+    MainHeaderComponent,
+    MainFooterComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
