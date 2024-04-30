73cabeb0be0bbdc285f835f13fd8a38dd507a197
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-29 22:19:43 -0400

    Mejora de footer

--- src/app/modules/main/pages/main-layout/main-layout.component.ts
+++ src/app/modules/main/pages/main-layout/main-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { CmmEcomFooterConfig } from 'src/app/common/data/utils/models/ecommerce.models';
+import { icons } from 'src/assets/images/image-routes';
 
 @Component({
   selector: 'pag-main-layout',
@@ -23,6 +25,48 @@ export class MainLayoutComponent {
    */
   smallMode: boolean = false
 
+  footerConfig: CmmEcomFooterConfig = {
+    items: [
+      {
+        icon: icons.van,
+        info: 'Envío Gratis',
+        text: 'Solo dentro la ciudad de Caracas'
+      },
+      {
+        icon: icons.return,
+        info: 'Devoluciones',
+        text: 'Devoluciones gratuitas y cambios'
+      },
+      {
+        icon: icons.purse,
+        info: 'Empaque exclusivo',
+        text: 'Empaque exclusivo por marca'
+      },
+    ],
+    businessInfo: {
+      name: 'MIRACULOUS',
+      phone: '+58 0424-5555607',
+      mail: 'atencion@miraculous.com',
+      direction: 'Av. Circumbalación del sol, calle 110 con av 62 de El Cafetal, Caracas'
+    },
+    products: [
+      'Ropa',
+      'Calzado',
+      'Accesorios',
+      'Damas',
+      'Caballeros',
+    ],
+    aboutUs: [
+      'Quienes somos',
+      'Ventas al mayor',
+      'Ofertas'
+    ],
+    socialNetworkIcons: [
+      icons.instagram,
+      icons.facebook,
+    ]
+  }
+
   @HostListener('window:resize')
   onResize(e: Event) {
 
@@ -31,8 +75,6 @@ export class MainLayoutComponent {
     } else {
       this.smallMode = false
     }
-
-    console.log(window.innerWidth);
 
   }
 
